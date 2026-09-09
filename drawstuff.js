9f969c433714afb7835a1135bdc67b397138d1ce
toxicfaIIs <apolloalexf@gmail.com>
2026-09-09 14:31:44 -0400

    Lil to the left.

--- drawstuff.js
+++ drawstuff.js
@@ -391,8 +391,8 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Define a rectangle in 2D with colors and coords at corners
-    var globals = { lightPos: new Vector(100,100,50),  // light over left upper rect
-                    lightCol: new Color(255,255,255)}; // light is white
+    var globals = { lightPos: new Vector(50,100,50),  // light over left upper rect
+                    lightCol: new Color(0,255,255)}; // light is white
     var tlAttribs = { diffuse: new Color(0,0,255)};    // all four rect verts blue
     var trAttribs = { diffuse: new Color(0,0,255)};
     var brAttribs = { diffuse: new Color(0,0,255)};
